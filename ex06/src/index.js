20fb1590acaa93063aaff23dc88adc94ca1820e7
edelhida <edelhida.dervisevic@gmail.com>
2021-03-26 18:20:23 +0100

    Task06 added

---
+++ ex06/src/index.js
@@ -0,0 +1,19 @@
+// src/index.js 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends React.Component {
+  render() { 
+    const world = "World";
+
+    return (
+      <div className="App">
+        <h1>{world}</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
